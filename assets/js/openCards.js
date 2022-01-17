83c1c305cbc3c5a1f3270bff8a02f26ba8ac9c72
Elias Peeters <Elias.peeters@otto.de>
2022-01-17 13:22:07 +0100

    Added details button

---
+++ assets/js/openCards.js
@@ -0,0 +1,38 @@
+function openCard(element, index, content, open = true) {
+    let card = document.getElementsByClassName(`card_${index}`)[0];
+    let trafficLight = document.getElementsByClassName(`trafficLight_${index}`)[0];    
+    let description = document.getElementsByClassName(`description_p_${index}`)[0];
+    let moreButton = document.getElementsByClassName(`moreButton_${index}`)[0];
+    let moreButton_p = document.getElementsByClassName(`moreButton_p_${index}`)[0];
+
+    if (!card.classList.contains('open')) {
+        
+        card.classList.add('open');
+        
+
+        trafficLight.style.animation = 'open_trafficLight 300ms ease-in-out forwards';
+        card.style.animation = 'open_card 300ms ease-in-out forwards';
+        description.style.animation = 'open_Description 500ms ease-in-out forwards';
+        moreButton.style.animation = 'open_moreButton 500ms ease-in-out forwards';
+
+        description.innerHTML = content;
+        moreButton_p.innerHTML = 'Schließen';
+    } else {
+        card.classList.remove('open');
+
+        
+        card.style.animation = 'close_card 300ms ease-in-out forwards';
+        trafficLight.style.animation = 'close_trafficLight 300ms ease-in-out forwards';
+        description.style.animation = 'close_Description 500ms ease-in-out forwards';
+        moreButton.style.animation = 'close_moreButton 500ms ease-in-out forwards';
+        
+        description.innerHTML = '';
+        moreButton_p.innerHTML = 'Details';
+    }
+    //console.log(element)
+}
+
+function close(index) {
+    let trafficLight = document.getElementsByClassName(`trafficLight_${index}`)[0];
+    trafficLight.style.animation = 'null';
+}
