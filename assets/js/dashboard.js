6b1e0bcac40bb5f76ebc0377bd1ec026b819a842
Elias Peeters <Elias.peeters@me.com>
2022-04-24 17:10:55 +0200

    Added dashboard link functions

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,11 +3,14 @@ function sendSave(id, idDB, newCard = false) {
     let color = document.getElementsByClassName(`color${id}`)[0].value;
     let description = document.getElementsByClassName(`description${id}`)[0].value;
     let orderNumber = document.getElementsByClassName(`order${id}`)[0].value;
+    let buttonCaption = document.getElementsByClassName(`buttonCaption${id}`)[0].value;
+    let buttonLink = document.getElementsByClassName(`buttonLink${id}`)[0].value;
+    
     
     if (newCard) {
         window.location = `/new?heading=${heading}&color=${color}&description=${description}`    
     } else {
-        window.location = `/save?id=${idDB}&heading=${heading}&color=${color}&description=${description}&orderNumber=${orderNumber}`
+        window.location = `/save?id=${idDB}&heading=${heading}&color=${color}&description=${description}&orderNumber=${orderNumber}&buttonLink=${buttonLink}&buttonCaption=${buttonCaption}`
     }
 }
 
